134814c43d8afff375fbdea6c971662f1af6f242
Silvan Loser <silvan.loser@hotmail.ch>
2018-02-27 19:33:20 +0100

    added comments to interface

--- src/main/java/ch/hslu/ad/sw01/ex01/Memory.java
+++ src/main/java/ch/hslu/ad/sw01/ex01/Memory.java
@@ -2,11 +2,28 @@ package ch.hslu.ad.sw01.ex01;
 
 public interface Memory {
 
+
+    /**
+     * Allocate block on Memory with given size.
+     * @param prefered size of block
+     */
     Allocation malloc(int blockSize);
 
+    /**
+     * Free Memory of Allocation added as parameter.
+     * @param specific allocation the be deleted
+     */
     void free(Allocation block);
 
+    /**
+     * Get usage of memory
+     * @return integer value of used
+     */
     int getUsed();
 
+    /**
+     * Returns the size of the memory
+     * @return memory size
+     */
     int getSize();
 }
